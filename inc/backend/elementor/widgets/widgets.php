bf0b4272510877a3a7b451ffd3a0ea51ef4e6b11
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-11-01 15:19:04 +0500

    sdfsdf

--- inc/backend/elementor/widgets/widgets.php
+++ inc/backend/elementor/widgets/widgets.php
@@ -38,3 +38,4 @@ locate_template('/inc/backend/elementor/widgets/food-box.php', true, true);
 locate_template('/inc/backend/elementor/widgets/woocommerce-products.php', true, true);
 locate_template('/inc/backend/elementor/widgets/text-marquee.php', true, true);
 locate_template('/inc/backend/elementor/widgets/image-box-2.php', true, true);
+locate_template('/inc/backend/elementor/widgets/dynamic-title.php', true, true);
