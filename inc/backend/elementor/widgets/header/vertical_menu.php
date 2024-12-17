df9d80e1f142723b1ed3f7f6b1bba1bdc95ec8cc
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-12-17 15:37:51 +0500

    sdfsdf

--- inc/backend/elementor/widgets/header/vertical_menu.php
+++ inc/backend/elementor/widgets/header/vertical_menu.php
@@ -45,123 +45,6 @@ class Restobar_Vertical_Menu extends Widget_Base{
 			]
 		);
 
-		// Left Icon Options
-		$this->add_control(
-			'show_left_icon',
-			[
-				'label' => __( 'Show Left Icon', 'restobar' ),
-				'type' => Controls_Manager::SWITCHER,
-				'label_on' => __( 'Show', 'restobar' ),
-				'label_off' => __( 'Hide', 'restobar' ),
-				'return_value' => 'yes',
-				'default' => 'no',
-			]
-		);
-
-		$this->add_control(
-			'left_icon',
-			[
-				'label' => __( 'Left Icon', 'restobar' ),
-				'type' => Controls_Manager::ICONS,
-				'default' => [
-					'value' => 'fas fa-chevron-right',
-					'library' => 'fa-solid',
-				],
-				'condition' => [
-					'show_left_icon' => 'yes',
-				],
-			]
-		);
-
-		// Right Icon Options
-		$this->add_control(
-			'show_right_icon',
-			[
-				'label' => __( 'Show Right Icon', 'restobar' ),
-				'type' => Controls_Manager::SWITCHER,
-				'label_on' => __( 'Show', 'restobar' ),
-				'label_off' => __( 'Hide', 'restobar' ),
-				'return_value' => 'yes',
-				'default' => 'no',
-			]
-		);
-
-		$this->add_control(
-			'right_icon',
-			[
-				'label' => __( 'Right Icon', 'restobar' ),
-				'type' => Controls_Manager::ICONS,
-				'default' => [
-					'value' => 'fas fa-chevron-right',
-					'library' => 'fa-solid',
-				],
-				'condition' => [
-					'show_right_icon' => 'yes',
-				],
-			]
-		);
-
-		$this->end_controls_section();
-
-		// Icon Style Section
-		$this->start_controls_section(
-			'icon_style_section',
-			[
-				'label' => __( 'Icon Style', 'restobar' ),
-				'tab'   => Controls_Manager::TAB_STYLE,
-			]
-		);
-
-		// Left Icon Color
-		$this->add_control(
-			'left_icon_color',
-			[
-				'label' => __( 'Left Icon Color', 'restobar' ),
-				'type' => Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .vertical-services-menu li a .left-icon' => 'color: {{VALUE}};',
-				],
-				'condition' => [
-					'show_left_icon' => 'yes',
-				],
-			]
-		);
-
-		// Right Icon Color
-		$this->add_control(
-			'right_icon_color',
-			[
-				'label' => __( 'Right Icon Color', 'restobar' ),
-				'type' => Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .vertical-services-menu li a .right-icon' => 'color: {{VALUE}};',
-				],
-				'condition' => [
-					'show_right_icon' => 'yes',
-				],
-			]
-		);
-
-		// Icon Size
-		$this->add_responsive_control(
-			'icon_size',
-			[
-				'label' => __( 'Icon Size', 'restobar' ),
-				'type' => Controls_Manager::SLIDER,
-				'size_units' => [ 'px', 'em', 'rem' ],
-				'range' => [
-					'px' => [
-						'min' => 10,
-						'max' => 100,
-					],
-				],
-				'selectors' => [
-					'{{WRAPPER}} .vertical-services-menu li a .left-icon, 
-					 {{WRAPPER}} .vertical-services-menu li a .right-icon' => 'font-size: {{SIZE}}{{UNIT}};',
-				],
-			]
-		);
-
 		$this->end_controls_section();
 
 		// Style Section for Vertical Menu
@@ -258,88 +141,21 @@ class Restobar_Vertical_Menu extends Widget_Base{
    	protected function render() {
 		$settings = $this->get_settings_for_display();
 		$active_mmenu = in_array('xp_mega-menu/xp_mega-menu.php', apply_filters('active_plugins', get_option('active_plugins')));
-		
-		// Prepare icon classes and conditions
-		$show_left_icon = $settings['show_left_icon'] === 'yes';
-		$show_right_icon = $settings['show_right_icon'] === 'yes';
 	?>
-		<nav class="vertical-services-navigation">			
-			<?php
-			// Custom walker to add icons
-			class Vertical_Menu_With_Icons_Walker extends \Walker_Nav_Menu {
-				private $settings;
-
-				public function __construct($settings) {
-					$this->settings = $settings;
-				}
-
-				public function start_el( &$output, $item, $depth = 0, $args = null, $id = 0 ) {
-					// Start parent method
-					$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
-					$classes = empty( $item->classes ) ? array() : (array) $item->classes;
-					$classes[] = 'menu-item-' . $item->ID;
-
-					// Prepare class string
-					$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
-					$class_names = ' class="' . esc_attr( $class_names ) . '"';
-
-					// Start output
-					$output .= $indent . '<li' . $class_names .'>';
-
-					// Prepare link attributes
-					$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
-					$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
-					$attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
-					$attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
-
-					// Prepare left icon
-					$left_icon_html = '';
-					if ($this->settings['show_left_icon'] === 'yes') {
-						$left_icon = $this->settings['left_icon'];
-						$left_icon_html = sprintf(
-							'<span class="left-icon %s" aria-hidden="true"></span>',
-							esc_attr($left_icon['value'])
-						);
-					}
-
-					// Prepare right icon
-					$right_icon_html = '';
-					if ($this->settings['show_right_icon'] === 'yes') {
-						$right_icon = $this->settings['right_icon'];
-						$right_icon_html = sprintf(
-							'<span class="right-icon %s" aria-hidden="true"></span>',
-							esc_attr($right_icon['value'])
-						);
-					}
-
-					// Link output with icons
-					$item_output  = $args->before;
-					$item_output .= '<a' . $attributes . '>';
-					$item_output .= $left_icon_html;
-					$item_output .= '<span class="menu-item-text">' . $item->title . '</span>';
-					$item_output .= $right_icon_html;
-					$item_output .= '</a>';
-					$item_output .= $args->after;
-
-					$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-				}
-			}
-
-			// Prepare walker with current settings
-			$walker = new Vertical_Menu_With_Icons_Walker($settings);
-
-			wp_nav_menu( array(
-				'menu' 			 => $settings['nav_menu'],
-				'menu_id'        => 'services-menu',
-				'menu_class'     => 'vertical-services-menu',
-				'container'      => 'ul',
-				'theme_location' => '__no_such_location',
-				'fallback_cb'    => '__return_empty_string', 
-				'walker'         => $active_mmenu ? new \Xp_Mega_Menu_Walker() : $walker,
-			) );
-			?>
-		</nav>
-    <?php
+			<nav class="vertical-services-navigation">			
+				<?php
+					wp_nav_menu( array(
+						'menu' 			 => $settings['nav_menu'],
+						'menu_id'        => 'services-menu',
+						'menu_class'     => 'vertical-services-menu',
+						'container'      => 'ul',
+						'theme_location' => '__no_such_location',
+    					'fallback_cb'    => '__return_empty_string', 
+    					'walker'         => $active_mmenu ? new \Xp_Mega_Menu_Walker() : '',
+					) );
+				?>
+			</nav>
+	    <?php
 	}
 }
 
