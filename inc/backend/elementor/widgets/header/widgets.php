7589dacc874decde9c25ddef9f5dcd0bd1fed20d
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-12-17 14:39:56 +0500

    sdfsdf

--- inc/backend/elementor/widgets/header/widgets.php
+++ inc/backend/elementor/widgets/header/widgets.php
@@ -2,6 +2,7 @@
 // Include widget files for Elementor widgets
 locate_template('/inc/backend/elementor/widgets/header/logo.php', true, true);
 locate_template('/inc/backend/elementor/widgets/header/menu.php', true, true);
+locate_template('/inc/backend/elementor/widgets/header/vertical_menu.php', true, true);
 locate_template('/inc/backend/elementor/widgets/header/search.php', true, true);
 locate_template('/inc/backend/elementor/widgets/header/side-panel.php', true, true);
 locate_template('/inc/backend/elementor/widgets/header/menu-mobile.php', true, true);
